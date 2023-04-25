c55222a65a52cfda130477ac0d59e7ef5d98ea2c
sona <sona22hakobyan@gmail.com>
2023-04-25 17:14:34 +0400

    fronend continue

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react"
 
+// components
+import WorkoutDetails from '../components/WorkoutDetails'
+
 const Home = () => {
 const [workouts, setWorkouts] = useState(null)
 
+
+
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('http://localhost:4000/api/workouts')
+            const response = await fetch('/api/workouts')
             const json = await response.json
 
             if (response.ok){
@@ -15,13 +20,15 @@ const [workouts, setWorkouts] = useState(null)
         fetchWorkouts()
     }, [])
 
-    return(
+    return (
         <div className="home">
-            <div className="workouts">
-    
-            </div>
+          <div className="workouts">
+            {workouts && workouts.map(workout => (
+              <WorkoutDetails workout={workout} key={workout._id} />
+            ))}
+          </div>
         </div>
-    )
+      )
 }
 
 export default Home
